ca5896f54e96e8352ac69d182e0f533415e6c464
duy123450 <duy.h0098642@gmail.com>
2026-04-10 12:11:40 +0700

    13

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api/v1/wiki",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export const getMovieInfo = () => api.get("/movie-info").then((res) => res.data.movie);
