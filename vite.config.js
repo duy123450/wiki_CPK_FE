21007b54a59f3ee2b238b7629a7b54d82c1f6422
duy123450 <duy.h0098642@gmail.com>
2026-04-11 22:56:53 +0700

    15

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: {
+        safari: 14,
+      },
+    },
+  },
+
+  build: {
+    cssMinify: 'lightningcss',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+
   server: {
     proxy: {
       '/api': {
@@ -12,4 +46,4 @@ export default defineConfig({
       },
     },
   },
-})
+}) 
